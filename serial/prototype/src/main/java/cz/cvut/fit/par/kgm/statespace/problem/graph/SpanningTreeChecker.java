026bce693fc087dbe97f90345694724dd45b3f32
stoupa <vladimirkroupa@gmail.com>
2012-10-10 11:57:40 +0200

    simpler solution

--- serial/prototype/src/main/java/cz/cvut/fit/par/kgm/statespace/problem/graph/SpanningTreeChecker.java
+++
@@ -1,9 +0,0 @@
-package cz.cvut.fit.par.kgm.statespace.problem.graph;
-
-import cz.cvut.fit.par.kgm.statespace.problem.graph.domain.UndirectedGraph;
-
-public interface SpanningTreeChecker {
-
-	boolean isSpanningTreeOf(UndirectedGraph tree, UndirectedGraph graph);
-	
-}
